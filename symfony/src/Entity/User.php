57aa7daa9f716f03443849d389514ff3e644c448
nikolay vanzhin <nikolay.vanzhin@yandex.ru>
2022-10-30 12:34:20 +0500

    email uniq validation updated

--- symfony/src/Entity/User.php
+++ symfony/src/Entity/User.php
@@ -4,10 +4,17 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+/**
+ * @UniqueEntity(
+ *     fields={"email"},
+ *     message="User with this email is already registered!"
+ * )
+ */
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
